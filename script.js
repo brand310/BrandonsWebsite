3f290fdd3c4a19803054f86d00cb0079e6ecc3cd
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-06-26 01:43:49 -0700

    Added new navigation bar with menu options which has also been styled in CSS.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector ('#mobile-menu')
 const menuLinks = document.querySelector ('.navmenu')
-
+console.log ('hi')
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menu.classList.toggle('active');
