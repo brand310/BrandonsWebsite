10a73df26e263f2059ec7ad99f01443e0267f6f0
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-06-26 01:27:04 -0700

    added a new nav bar and background

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-const myBtn = document.getElementById ('myBtn')
-console.log('myBtn')
+const menu = document.querySelector ('#mobile-menu')
+const menuLinks = document.querySelector ('.navmenu')
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menu.classList.toggle('active');
+})
